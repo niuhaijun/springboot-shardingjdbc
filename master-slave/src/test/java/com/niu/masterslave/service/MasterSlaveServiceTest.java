298b8d3ecc2ffffd919926c17ae1b2f157f75837
牛海军 <niuhaijun@100tal.com>
2019-08-10 06:25:21 +0800

    注释

--- master-slave/src/test/java/com/niu/masterslave/service/MasterSlaveServiceTest.java
+++ master-slave/src/test/java/com/niu/masterslave/service/MasterSlaveServiceTest.java
@@ -1,8 +1,6 @@
 package com.niu.masterslave.service;
 
 import com.niu.masterslave.MasterSlaveApplication;
-import com.niu.masterslave.model.Order;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,45 +17,17 @@ public class MasterSlaveServiceTest {
   @Autowired
   private MaserSlaveService maserSlaveService;
 
+  // ------   create   ------  //
+
   /**
    * 非事务方法，走【master】
+   *
    */
   @Test
   public void create() {
 
     int result = maserSlaveService.create();
-    log.info("-------create 方法的执行结果 {}", result == 10);
-  }
-
-  /**
-   * 非事务方法，走【slave0】
-   */
-  @Test
-  public void read() {
-
-    int result = maserSlaveService.read();
-    log.info("--------read 方法的执行结果 {}", result == 10);
-  }
-
-  /**
-   * 非事务方法，走【master】
-   */
-  @Test
-  public void update() {
-
-    int result = maserSlaveService.update();
-    log.info("--------update 方法的执行结果 {}", result == 10);
-  }
-
-
-  /**
-   * 非事务方法，走【master】
-   */
-  @Test
-  public void delete() {
-
-    int result = maserSlaveService.delete();
-    log.info("--------delete 方法的执行结果 {}", result == 10);
+    log.info("-------方法的执行结果 {}", result == 10);
   }
 
   /**
@@ -67,6 +37,147 @@ public class MasterSlaveServiceTest {
   public void createAndRead() {
 
     int result = maserSlaveService.createAndRead();
-    log.info("--------createAndRead 方法的执行结果 {}", result == 0);
+    log.info("--------方法的执行结果 {}", result == 0);
   }
+
+  @Test
+  public void createAndUpdate() {
+
+    int result = maserSlaveService.createAndUpdate();
+    log.info("-------方法的执行结果 {}", result == 10);
+  }
+
+  @Test
+  public void createAndDelete() {
+
+    int result = maserSlaveService.createAndDelete();
+    log.info("-------方法的执行结果 {}", result == 10);
+  }
+
+  // ------   read   ------  //
+
+  /**
+   * 非事务方法，走【slave0】
+   */
+  @Test
+  public void read() {
+
+    int result = maserSlaveService.read();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【slave0】
+   */
+  @Test
+  public void readAndCreate() {
+
+    int result = maserSlaveService.readAndCreate();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【slave0】
+   */
+  @Test
+  public void readAndUpdate() {
+
+    int result = maserSlaveService.readAndUpdate();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【slave0】
+   */
+  @Test
+  public void readAndDelete() {
+
+    int result = maserSlaveService.readAndDelete();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  // ------   update   ------  //
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void update() {
+
+    int result = maserSlaveService.update();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void updateAndCreate() {
+
+    int result = maserSlaveService.updateAndCreate();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void updateAndRead() {
+
+    int result = maserSlaveService.updateAndRead();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void updateAndDelete() {
+
+    int result = maserSlaveService.updateAndDelete();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  // ------   delete   ------  //
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void delete() {
+
+    int result = maserSlaveService.delete();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void deleteAndCreate() {
+
+    int result = maserSlaveService.deleteAndCreate();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void deleteAndRead() {
+
+    int result = maserSlaveService.deleteAndRead();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
+  /**
+   * 非事务方法，走【master】
+   */
+  @Test
+  public void deleteAndUpdate() {
+
+    int result = maserSlaveService.deleteAndUpdate();
+    log.info("--------方法的执行结果 {}", result == 10);
+  }
+
 }
