57b98690ae1232d448afd39cd77b58362acbfd1a
牛海军 <niuhaijun@100tal.com>
2019-08-09 22:39:36 +0800

    解决Bean无法被增强问题

--- master-slave/src/main/java/com/niu/masterslave/config/MyBatisMapperScannerConfig.java
+++ master-slave/src/main/java/com/niu/masterslave/config/MyBatisMapperScannerConfig.java
@@ -14,8 +14,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyBatisMapperScannerConfig {
 
+  /**
+   * 2019-08-09 22:26:41,552 [restartedMain] INFO  o.s.c.a.ConfigurationClassPostProcessor [/] - [Cannot enhance @Configuration bean definition 'myBatisMapperScannerConfig' since its singleton instance has been created too early. The typical cause is a non-static @Bean method with a BeanDefinitionRegistryPostProcessor return type: Consider declaring such methods as 'static'.]
+   *
+   * 解决方案：使用static修饰bean方法
+   */
   @Bean
-  public MapperScannerConfigurer mapperScannerConfigurer() {
+  public static MapperScannerConfigurer mapperScannerConfigurer() {
 
     MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
     mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
