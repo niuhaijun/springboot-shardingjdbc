80f0909178742d001f6960b9aa3cc14f112754d4
牛海军 <niuhaijun@100tal.com>
2019-08-10 06:02:32 +0800

    实验

--- master-slave/src/main/java/com/niu/masterslave/service/MaserSlaveService.java
+++ master-slave/src/main/java/com/niu/masterslave/service/MaserSlaveService.java
@@ -7,13 +7,38 @@ package com.niu.masterslave.service;
  */
 public interface MaserSlaveService {
 
+  /**
+   * 创建以及其他组合
+   */
   int create();
-
-  int read();
-
-  int update();
-
-  int delete();
-
   int createAndRead();
+  int createAndUpdate();
+  int createAndDelete();
+
+
+  /**
+   * 查询以及其他组合
+   */
+  int read();
+  int readAndCreate();
+  int readAndUpdate();
+  int readAndDelete();
+
+
+  /**
+   * 更新以及其他组合
+   */
+  int update();
+  int updateAndCreate();
+  int updateAndRead();
+  int updateAndDelete();
+
+
+  /**
+   * 删除以及其他组合
+   */
+  int delete();
+  int deleteAndRead();
+  int deleteAndCreate();
+  int deleteAndUpdate();
 }
--- master-slave/src/main/java/com/niu/masterslave/service/impl/MaserSlaveServiceImpl.java
+++ master-slave/src/main/java/com/niu/masterslave/service/impl/MaserSlaveServiceImpl.java
@@ -5,7 +5,6 @@ import com.niu.masterslave.model.Order;
 import com.niu.masterslave.model.OrderExample;
 import com.niu.masterslave.service.MaserSlaveService;
 import com.niu.masterslave.utils.UUIDUtils;
-import java.util.List;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,13 +20,9 @@ public class MaserSlaveServiceImpl implements MaserSlaveService {
   @Autowired
   private OrderMapper orderMapper;
 
-  /**
-   * 插入10条数据
-   *
-   * order_id 和 user_id的范围都是[1, 10]
-   */
-  @Override
-  public int create() {
+
+  // ----------   CRUD --------- //
+  private int c() {
 
     int result = 0;
 
@@ -49,14 +44,12 @@ public class MaserSlaveServiceImpl implements MaserSlaveService {
     return result;
   }
 
-  @Override
-  public int read() {
+  private int r() {
 
     return orderMapper.countByExample(new OrderExample());
   }
 
-  @Override
-  public int update() {
+  private int u() {
 
     Order record = new Order();
     record.setOrderId(100L);
@@ -67,35 +60,150 @@ public class MaserSlaveServiceImpl implements MaserSlaveService {
     return orderMapper.updateByExampleSelective(record, example);
   }
 
-  @Override
-  public int delete() {
+  private int d() {
 
     return orderMapper.deleteByExample(new OrderExample());
+  }
 
+
+  // ----------   创建以及组合方法 --------- //
+  @Override
+  public int create() {
+
+    return c();
   }
 
   @Override
   public int createAndRead() {
 
     int result = 0;
+    result += c();
+    result += r();
+    return result;
+  }
 
-    Random random = new Random();
+  @Override
+  public int createAndUpdate() {
 
-    long[] orderIds = random.longs(1, 10).limit(10).toArray();
-    long[] numbers = random.longs(1, 10).limit(10).toArray();
-    for (int m = 0; m < 10; m++) {
-      Long orderId = orderIds[m];
-      Long number = numbers[m];
-      Order record = new Order();
-      record.setUuid(UUIDUtils.getUUID());
-      record.setOrderId(orderId);
-      record.setUserId(number);
-      record.setContent("test " + number);
-      result += orderMapper.insert(record);
-    }
+    int result = 0;
+    result += c();
+    result += u();
+    return result;
 
-    List<Order> list = orderMapper.selectByExample(new OrderExample());
-    result += list.size();
+  }
+
+  @Override
+  public int createAndDelete() {
+
+    int result = 0;
+    result += c();
+    result += d();
+    return result;
+  }
+
+
+  // ----------   查询以及组合方法 --------- //
+  @Override
+  public int read() {
+
+    return r();
+
+  }
+
+  @Override
+  public int readAndCreate() {
+
+    int result = 0;
+    result += r();
+    result += c();
+    return result;
+  }
+
+  @Override
+  public int readAndUpdate() {
+
+    int result = 0;
+    result += r();
+    result += u();
+    return result;
+  }
+
+  @Override
+  public int readAndDelete() {
+
+    int result = 0;
+    result += r();
+    result += d();
+    return result;
+  }
+
+
+  // ----------   更新以及组合方法 --------- //
+  @Override
+  public int update() {
+
+    return u();
+  }
+
+  @Override
+  public int updateAndCreate() {
+
+    int result = 0;
+    result += u();
+    result += c();
+    return result;
+  }
+
+  @Override
+  public int updateAndRead() {
+
+    int result = 0;
+    result += u();
+    result += r();
+    return result;
+  }
+
+  @Override
+  public int updateAndDelete() {
+
+    int result = 0;
+    result += u();
+    result += d();
+    return result;
+  }
+
+
+  // ----------   删除以及组合方法 --------- //
+  @Override
+  public int delete() {
+
+    return d();
+  }
+
+  @Override
+  public int deleteAndRead() {
+
+    int result = 0;
+    result += d();
+    result += r();
+    return result;
+  }
+
+  @Override
+  public int deleteAndCreate() {
+
+    int result = 0;
+    result += d();
+    result += c();
+    return result;
+  }
+
+  @Override
+  public int deleteAndUpdate() {
+
+    int result = 0;
+    result += d();
+    result += u();
     return result;
   }
 
