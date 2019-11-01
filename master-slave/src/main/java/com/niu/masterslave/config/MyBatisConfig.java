4b60b8024ac295efd96ccaa3038b02360632666f
牛海军 <niuhaijun@100tal.com>
2019-11-01 14:20:33 +0800

    删除无用包

--- master-slave/src/main/java/com/niu/masterslave/config/MyBatisConfig.java
+++ master-slave/src/main/java/com/niu/masterslave/config/MyBatisConfig.java
@@ -1,8 +1,6 @@
 package com.niu.masterslave.config;
 
 import com.github.pagehelper.PageInterceptor;
-import io.shardingsphere.api.config.rule.MasterSlaveRuleConfiguration;
-import java.util.Arrays;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.ibatis.plugin.Interceptor;
