998d0d68065af99518aa51a279ddd01a095eb9e8
牛海军 <niuhaijun@100tal.com>
2019-11-01 15:04:50 +0800

    单库分表实战

---
+++ sharding-table/src/test/java/com/niu/shardingtable/ShardingTableApplicationTests.java
@@ -0,0 +1,14 @@
+package com.niu.shardingtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingTableApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
